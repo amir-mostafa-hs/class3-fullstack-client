2f3cf63931312b30fb3bfc4cb5350a2a2618fb4f
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2023-03-04 20:45:09 +0330

    edit url

--- src/API/courseApi.js
+++ src/API/courseApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BaseUrl = "http://localhost:5050/course";
+const BaseUrl = "https://fullstack-server-ds8z.onrender.com//course";
 
 const DeleteCourseApi = async (id) => {
   const result = await axios.delete(BaseUrl + "/" + id);
